b861a25447c44a1e54a1578c58b118f68b0fc005
xxj15 <seojini00@naver.com>
2025-01-28 23:04:00 +0900

    1037(약수).py

---
+++ BAEKJOON/BOJ_BETA/미해결/6603.py
@@ -0,0 +1,11 @@
+import sys 
+input = sys.stdin.readline
+
+while True:
+        line = input().strip()
+        data = list(map(int, line.split()))
+
+        k = data[0] # 골라야하는 숫자 개수 
+        s = data[1:] # 숫자들
+        if data[0] == 0:
+            break
