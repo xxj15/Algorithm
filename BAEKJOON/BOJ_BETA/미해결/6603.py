8bfb84947d2e5614059f223948342d3ee9e4e294
xxj15 <seojini00@naver.com>
2025-01-29 14:33:54 +0900

    1929(소수 구하기).py

--- BAEKJOON/BOJ_BETA/미해결/6603.py
+++ BAEKJOON/BOJ_BETA/미해결/6603.py
@@ -6,6 +6,8 @@ while True:
         data = list(map(int, line.split()))
 
         k = data[0] # 골라야하는 숫자 개수 
-        s = data[1:] # 숫자들
+        s = data[1:] # 후보 숫자들
         if data[0] == 0:
             break
+
+        
