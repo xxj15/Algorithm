23915c2d57c9467f24944267e090e1e2724d1a35
xxj15 <seojini00@naver.com>
2025-02-03 16:53:01 +0900

    2580(스도쿠).py

--- BAEKJOON/BOJ_BETA/골드/2661.py
+++ BAEKJOON/BOJ_BETA/골드/2661.py
@@ -14,14 +14,14 @@ def check(sequence):
     return 0 
 
 def dfs(depth):
-    if depth == n:  # 원하는 길이에 도달했으면 출력
+    if depth == n:
         print(''.join(map(str, ans)))
-        exit(0)  # 프로그램 종료
+        exit(0) 
     
-    for x in range(1, 4):  # 1, 2, 3을 시도
-        ans.append(x)  # 값을 추가
+    for x in range(1, 4): 
+        ans.append(x) 
         if check(ans) == 0:  # 좋은 수열이면
             dfs(depth + 1)  # 다음 단계로 재귀 호출
-        ans.pop()  # 값을 제거하여 백트래킹
+        ans.pop()  
 
 dfs(0)
