1ac81a75440bc700d25a7683eeef3fd28fd717ee
xxj15 <seojini00@naver.com>
2025-03-25 16:56:48 +0900

    1339(단어 수학)

--- Study/알고리즘 기초/1339.py
+++ Study/알고리즘 기초/1339.py
@@ -1,33 +1,31 @@
 #단어 수학
 import sys
 input = sys.stdin.readline
-from collections import defaultdict
 
-n = int(input())
-words = [input().strip() for _ in range(n)]
+N = int(input()) # 단어 개수 입력받기 
+words=[input().strip() for _ in range(N)] 
 
-weight = defaultdict(int)
+weight = {} # 가중치 저장 
 
-# 각 알파벳의 자릿수 가중치 계산
+# 가중치 포함해서 저장 
 for word in words:
     length = len(word)
-    for i, char in enumerate(word):
-        weight[char] += 10 ** (length - i - 1)
+    for i in range(length):
+        char = word[i]
 
-# 가중치 기준으로 정렬
-sorted_chars = sorted(weight.items(), key=lambda x: -x[1])
+        if char not in weight:
+            weight[char] = 10**(length - i-1)
 
-# 9부터 숫자 할당
-num = 9
-char_to_digit = {}
-for char, _ in sorted_chars:
-    char_to_digit[char] = num
+        else: 
+            weight[char] += 10**(length - i-1)
+
+sorted_weights = sorted(weight.items(), key=lambda x: x[1], reverse=True)     
+
+ans = 0
+num = 9 
+
+for key, value in sorted_weights:
+    ans += value * num
     num -= 1
 
-# 단어를 숫자로 변환하고 합 계산
-total = 0
-for word in words:
-    converted = ''.join(str(char_to_digit[char]) for char in word)
-    total += int(converted)
-
-print(total)
+print(ans)
